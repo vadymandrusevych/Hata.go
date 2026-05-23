97075b9594d2e342e94920fece0c072b9de1e4bb
vadymandrusevych <286009824+vadymandrusevych@users.noreply.github.com>
2026-05-23 18:31:45 +0200

    Почав проект фронтенду

---
+++ Frontend/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+	<script>
+
+	import { resolve } from '$app/paths';
+	</script>
+
+
+	<div id="nav-menu" class= "flex justify-between flex-row  w-full font-bold bg-blue-300 h-auto p-6" >
+		<div id="Header" class="text-7xl h-full w-auto text overflow-hidden min-w-0 content-center text-center"  >
+			<a class="block truncate w-full" href={resolve('/')}>
+				HATA.GO
+			</a>
+		</div>
+<!--		<div id="separator" class="bg-amber-500  w-auto h-full">  </div>  -->
+		<nav class="h-12 content-center">
+		<a id="Add Listing" class="bg-yellow-200  w-auto text-center text-2xl h-full m-3 py-4 px-10 rounded-xl "> Add-Listing </a>
+		<a id="Regbutton" class="bg-yellow-100 text-center text-2xl h-full m-3 py-4 px-10 rounded-xl"> Register </a>
+		<a id="Logbutton" class="bg-yellow-100 w-auto text-center text-2xl h-full m-3 py-4 px-10 rounded-xl content-centerD" > Login </a>
+
+		</nav>
+	</div >
+
+	<p class="bg-amber-700 w-full">hui</p>
+
+	<!--Треба флекс контент 💀💀💀 алаймент-->
---
+++ Frontend/src/routes/feed/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+
+	import { resolve } from '$app/paths';
+</script>
+
+<div id="nav-menu" class= "inline-flex">
+	<div class="text-2xl"  id="Header">
+		<nav>
+			<a href={resolve('/')}>
+				HATA.GO
+			</a>
+		</nav>
+	</div>
+	<div class="bg-amber-500 w-4 h-4">  </div>
+</div >
