5ab8782813fdad1e7b5c72075ed0bd8771ebaeaf
vadymandrusevych <286009824+vadymandrusevych@users.noreply.github.com>
2026-05-23 19:44:20 +0200

    Поправив хоум фронтенду

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -4,16 +4,16 @@
 	</script>
 
 
-	<div id="nav-menu" class= "flex justify-between flex-row  w-full font-bold bg-blue-300 h-auto p-6" >
-		<div id="Header" class="text-7xl h-full w-auto text overflow-hidden min-w-0 content-center text-center"  >
-			<a class="block truncate w-full" href={resolve('/')}>
+	<div id="nav-menu" class= "flex justify-between flex-row h-auto w-full font-bold bg-blue-300 p-6 content-center" >
+		<div id="Header" class="text-7xl h-auto w-auto text overflow-hidden min-w-0 content-center "  >
+			<a class="block truncate w-full text-center allign-middle" href={resolve('/')}>
 				HATA.GO
 			</a>
 		</div>
 <!--		<div id="separator" class="bg-amber-500  w-auto h-full">  </div>  -->
-		<nav class="h-12 content-center">
+		<nav class="h-auto content-center-safe">
 		<a id="Add Listing" class="bg-yellow-200  w-auto text-center text-2xl h-full m-3 py-4 px-10 rounded-xl "> Add-Listing </a>
-		<a id="Regbutton" class="bg-yellow-100 text-center text-2xl h-full m-3 py-4 px-10 rounded-xl"> Register </a>
+		<a id="Regbutton" class="bg-yellow-100 w-auto text-center text-2xl h-full m-3 py-4 px-10 rounded-xl"> Register </a>
 		<a id="Logbutton" class="bg-yellow-100 w-auto text-center text-2xl h-full m-3 py-4 px-10 rounded-xl content-centerD" > Login </a>
 
 		</nav>
